1569575c596f185c6c186639deac722b412aa583
filipavinagre <filipa.vngr@gmail.com>
2025-03-24 23:18:09 +0000

    task05

--- task05/weekday.py
+++ task05/weekday.py
@@ -1,3 +1,19 @@
 # weekday.py
 # This program outputs whether or not today is a weekday.
 # Author: Filipa Vinagre
+
+# Import datetime
+from datetime import datetime
+
+dt = datetime.now()
+
+wd = dt.weekday()
+
+if wd < 5:
+    print ("Yes, unfortunately today is a week day!")
+else:
+    print ("yay, it's the weekend!")
+
+
+
+# Reference: Weekday function used from https://pynative.com/python-get-the-day-of-week/
